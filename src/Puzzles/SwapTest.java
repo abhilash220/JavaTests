c79e56dc4c371e85522e3969f2ad42481328f3b6
abhilash220 <abhilash220@gmail.com>
2024-02-08 23:27:02 -0500

    Variable swap objective

--- src/Puzzles/SwapTest.java
+++
@@ -1,5 +0,0 @@
-package Puzzles;
-
-public class SwapTest {
-    
-}
---
+++ src/interfaces/PuzzleInterface.java
@@ -0,0 +1,14 @@
+package interfaces;
+public interface PuzzleInterface {
+    /**
+     *Abstract method to swap the values of two variables using a 3rd variable
+     * @throws Exception
+     */
+    void swapVariableMethod() throws Exception;
+
+    /**
+     * Abstract method to swap the values of two variables without a 3rd variable
+     * @throws Exception
+     */
+    void swapNoVariableMethod() throws Exception;
+}
